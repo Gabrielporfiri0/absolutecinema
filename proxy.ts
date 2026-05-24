3673d3936989a8673a92901c1ba2d80699758f6d
LucasHenrique569 <cs206838@gmail.com>
2026-05-24 15:52:34 -0400

    Adiciona novo middleware

--- proxy.ts
+++ proxy.ts
@@ -2,17 +2,49 @@ import { NextRequest, NextResponse } from "next/server";
 import { verifyToken } from "./lib/jwt_";
 
 export async function proxy(request: NextRequest) {
-    const tokenExists = request.cookies.get('accessToken')?.value
+    const token = request.cookies.get('accessToken')?.value
+    const path = request.nextUrl.pathname
 
-    if (!tokenExists) return NextResponse.redirect(new URL('/admin/login', request.url))
+    // usuário está tentando acessar login
+    if (path === '/admin/login') {
+        if (!token) {
+            return NextResponse.next()
+        }
 
-    const tokenIsValid = await verifyToken(tokenExists)
+        const tokenIsValid = await verifyToken(token)
 
-    if (!tokenIsValid.valid) return NextResponse.redirect(new URL('/', request.url))
+        if (tokenIsValid.valid) {
+            return NextResponse.redirect(
+                new URL('/admin/dashboard', request.url)
+            )
+        }
+
+        return NextResponse.next()
+    }
+
+    // proteger dashboard
+    if (path.startsWith('/admin/dashboard')) {
+        if (!token) {
+            return NextResponse.redirect(
+                new URL('/admin/login', request.url)
+            )
+        }
+
+        const tokenIsValid = await verifyToken(token)
+
+        if (!tokenIsValid.valid) {
+            return NextResponse.redirect(
+                new URL('/admin/login', request.url)
+            )
+        }
+    }
 
     return NextResponse.next()
 }
 
 export const config = {
-    matcher: ['/admin/dashboard']
+    matcher: [
+        '/admin/login',
+        '/admin/dashboard/:path*'
+    ]
 }
